a48d2798f0e40255e205a02a69e4565c5513edf4
curiouscosmos <onecuriouscosmos@gmail.com>
2026-09-07 17:38:19 -0700

    Fix collapsed player transparency

--- application/vlc-android/src/org/videolan/vlc/gui/audio/AudioPlayerAnimator.kt
+++ application/vlc-android/src/org/videolan/vlc/gui/audio/AudioPlayerAnimator.kt
@@ -268,6 +268,9 @@ internal class AudioPlayerAnimator : IAudioPlayerAnimator, LifecycleObserver {
     override fun onSlide(slideOffset: Float) {
         if (inSearch) return
         binding.contentLayout.setBackgroundResource(if (slideOffset == 0f) android.R.color.transparent else R.drawable.audio_player_screen_background)
+        binding.backgroundView.alpha = slideOffset
+        binding.topGradient.alpha = slideOffset
+        binding.bottomGradient.alpha = slideOffset
         binding.progressBar.alpha = 1 - slideOffset
         binding.progressBar.layoutParams.height = ((1 - slideOffset) * 4.dp).toInt()
         binding.progressBar.requestLayout()
